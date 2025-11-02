5b6b9d81d40728e33df3f7c2c0c46e2cdb2eec82
Milan Belal <mohitbelal9@gmail.com>
2025-11-02 20:30:55 +0545

    learned til event handling

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,52 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const user = {
+    userName: "Milan Belal",
+    email: "milanbelal007@gmail.com",
+    age: 23
+  }
 
+  const userArr = ['HAri', 'Ram', 'MAdhav']
+  function Fruit() {
+    return "Apple"
+  }
+  const imgPath="https://images.pexels.com/photos/1633522/pexels-photo-1633522.jpeg"
+  function operation(a, b, op) {
+
+    if (op == "+") {
+      return a + b
+    }
+    else if (op == "-") {
+      return a - b
+    }
+    return a * b
+  }
+
+  function callMe(){
+     alert("functon called")
+  }
+
+  const hello=()=>{
+    alert("hello")
+  }
+
+  const god=(name)=>{
+    alert(name)
+  }
   return (
-    <h1>hello react</h1>
+
+    <div><h1>hello react</h1>
+      <h2>My name is {user.userName ? user.userName : "name not found"}</h2>
+      <h3>I love {Fruit()}</h3>
+      <h3>the operation result is {operation(10, 30)}</h3>
+      the third user is {userArr[2]}
+      <br />
+
+      {/* <img src={imgPath} alt="img here" /> */}
+      <button onClick={callMe}>click me</button>
+      <button onClick={()=>god("shiva")}>Shiva</button>
+    </div>
   )
 }
 
