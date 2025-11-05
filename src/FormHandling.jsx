a4166868b9a5177b0d3593d0c979c90d1ef65287
Milan Belal <mohitbelal9@gmail.com>
2025-11-05 21:12:18 +0545

    handled the radio button and select dropdown

--- src/FormHandling.jsx
+++ src/FormHandling.jsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 
 function FormHandling() {
     const [skills, setSkills] = useState([])
+    const [gender,setGender]=useState("male")
+    const [city,setCity]=useState('Dipayal')
     const handleSkills=(event) => {
         console.log(event.target.value)
         if(event.target.checked){
@@ -22,8 +24,20 @@ function FormHandling() {
                 <label htmlFor="java">JAVA</label><br />
                 <input onChange={handleSkills} type="checkbox" id="js" value="js" />
                 <label htmlFor="js">JS</label>
+                 <h1>{skills.toString()}</h1>
+                <h4>select Gender :{gender}</h4>
+                <input checked={gender=="male"} type="radio" name="gender" id="male" value={"male"} onChange={(event)=>setGender(event.target.value)} />Male
+                <input checked={gender=="female"} type="radio" name="gender" id="female" value={"female"} onChange={(event)=>setGender(event.target.value)} />FeMale
+                <hr />
+                <h4>select city</h4>
+                <select name="" id="" defaultValue={"ktm"} onChange={(event)=>setCity(event.target.value)}>
+                    <option  value="dhangadhi">Dhangadhi</option>
+                    <option value="dipayal">Dipayal</option>
+                    <option value="ktm">Kathmandu</option>
+                </select>
+                <h2>{city}</h2>
             </form>
-            <h1>{skills.toString()}</h1>
+           
         </div>
     )
 }
