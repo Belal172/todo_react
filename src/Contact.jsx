dbbb58c953db19af47378723ce95e800fc1be72f
Milan Belal <mohitbelal9@gmail.com>
2025-11-15 19:42:34 +0545

    fetch data using dummy api

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,9 +1,34 @@
-function Contact() {
+import { useEffect, useState } from "react"
+
+export default function Contact() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    getUserData()
+  }, [])
+  async function getUserData() {
+    const url = "https://dummyjson.com/users"
+    let response = await fetch(url)
+    response = await response.json()
+    console.log(response)
+    setData(response.users)
+
+  }
   return (
     <>
-     <h2>Contact page</h2>
+      <h1>Users data</h1>
+      {
+        data && data.map((user,index) => (
+          <ul key={index} className="list">
+            <li>{user.firstName}</li>
+            <li>{user.lastName}</li>
+            <li>{user.age}</li>
+            
+          </ul>
+        ))
+
+      }
+
     </>
   )
 }
 
-export default Contact
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,9 @@
 function Home() {
   return (
     <>
-    <h1>Home page</h1>
+     <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
     </>
   )
 }
