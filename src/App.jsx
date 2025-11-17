10cc907802a7d40f8a996031d0464b6a30012b8d
Milan Belal <mohitbelal9@gmail.com>
2025-11-17 21:03:03 +0545

    installed and configured the redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
-import About from "./About"
-import Contact from "./Contact"
-import Home from "./Home"
-import Navbar from "./Navbar"
-import { Routes ,Route,Link } from "react-router"
-import PageNotFound from "./PageNotFound"
+
 //import './App.css'
 function App() {
   return (
     <>
-    <Navbar/>
+    <h1>Redux toolkit learning</h1>
     
    
-     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="/*" element={<PageNotFound/>}/>
-     </Routes>
     </>
   )
 }
